f47eda436eb73f59ea629458cd84217379efe425
Xander Sanger <xandersangerwork@gmail.com>
2024-12-05 11:13:19 +1100

    refactor

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,3 @@
-
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where 
@@ -7,73 +6,33 @@ import qualified GI.Gtk as Gtk
 import Data.GI.Base
 import qualified Data.Text as T
 import System.Process
+import Data.Int
 
+type Command = String
 type Name = String
 type Image = FilePath
-type ButtonList =  [(Name,Image)]
+type ButtonList =  [(Name, Image, Command)]
 
-buttonList :: Gtk.Grid -> Gtk.Window -> ButtonList -> IO ()
-buttonList grid win buttonlist = mapM_ (uncurry (mkButton  grid win) ) buttonlist
-
-mkButton :: Gtk.Grid -> Gtk.Window -> Name -> Image -> IO () 
-mkButton grid win name image = do
-
+mkButtons :: Gtk.Grid -> Gtk.Window -> ButtonList -> IO ()
+mkButtons grid win buttonlist = mapM_ (\(position, (name, image, command)) -> do
     img <- Gtk.imageNewFromFile image
     btn <- Gtk.buttonNew
     Gtk.buttonSetRelief btn Gtk.ReliefStyleNone
     Gtk.buttonSetImage btn $ Just img
     label <- Gtk.labelNew Nothing
     Gtk.labelSetMarkup label  $ T.pack ("<b>"++  name ++ "</b>")
-    Gtk.widgetSetHexpand btn False
-    case name of 
-           "Cancel" -> do  
-              btn `on` #clicked $ Gtk.mainQuit
-              Gtk.gridAttach grid btn 0 0 1 1
-              Gtk.gridAttach grid label 0 1 1 1
-           "Hibernate" -> do 
-              btn `on` #clicked $ do
-                  callCommand  "sudo systemctl hibernate"
-                  Gtk.mainQuit
-              Gtk.gridAttach grid btn 1 0 1 1
-              Gtk.gridAttach grid label 1 1 1 1
-           "Lock" -> do 
-              btn `on` #clicked $ do
-                  callCommand  "loginctl lock-session"
-                  Gtk.mainQuit
-              Gtk.gridAttach grid btn 2 0 1 1
-              Gtk.gridAttach grid label 2 1 1 1
-           "Logout" -> do 
-              btn `on` #clicked $ do
-                  callCommand  "uwsm stop"
-                  Gtk.mainQuit
-              Gtk.gridAttach grid btn 3 0 1 1
-              Gtk.gridAttach grid label 3 1 1 1
-           "Reboot" -> do 
-              btn `on` #clicked $ do
-                  callCommand  "sudo reboot now"
-                  Gtk.mainQuit
-              Gtk.gridAttach grid btn 4 0 1 1
-              Gtk.gridAttach grid label 4 1 1 1
-           "Shutdown" -> do 
-              btn `on` #clicked $ do
-                  callCommand  "sudo shutdown now"
-                  Gtk.mainQuit
-              Gtk.gridAttach grid btn 5 0 1 1
-              Gtk.gridAttach grid label 5 1 1 1
-           "Suspend" -> do 
-              btn `on` #clicked $ do
-                  callCommand  "loginctl lock-session && sudo systemctl suspend"
-                  Gtk.mainQuit
-              Gtk.gridAttach grid btn 6 0 1 1
-              Gtk.gridAttach grid label  6 1 1 1
+    Gtk.widgetSetHexpand btn False 
+    btn `on` #clicked $ do
+        callCommand command
+        Gtk.mainQuit
+    Gtk.gridAttach grid btn position 0 1 1
+    Gtk.gridAttach grid label position 1 1 1
     return ()
-                            
-          
-    
+    ) (zip [0..] buttonlist)            
+              
 main :: IO ()
 main = do 
-    Gtk.init Nothing
-    
+    Gtk.init Nothing    
     win <- Gtk.windowNew Gtk.WindowTypeToplevel
     Gtk.setContainerBorderWidth win 10
     Gtk.setWindowTitle win "ByeBye"
@@ -88,20 +47,14 @@ main = do
     Gtk.gridSetRowSpacing grid 10
     Gtk.gridSetColumnHomogeneous grid True
     Gtk.containerAdd win grid
-    buttonList grid win [ ("Cancel","./img/cancel.png"),
-                    ("Hibernate","./img/hibernate.png"),
-                    ("Lock","./img/lock.png"),
-                    ("Logout","./img/logout.png"),
-                    ("Reboot","./img/reboot.png"),
-                    ("Shutdown","./img/shutdown.png"),
-                    ("Suspend","./img/suspend.png")
+    mkButtons grid win [ ("Cancel","./img/cancel.png",""),
+                    ("Hibernate","./img/hibernate.png","sudo systemctl hibernate"),
+                    ("Lock","./img/lock.png","loginctl lock-session"),
+                    ("Logout","./img/logout.png","loginctl terminate-user ''"),
+                    ("Reboot","./img/reboot.png","sudo reboot now"),
+                    ("Shutdown","./img/shutdown.png","sudo shutdown now"),
+                    ("Suspend","./img/suspend.png","sudo systemctl suspend")
                     ]
-
-    
-    
-    
-
-
     Gtk.onWidgetDestroy win Gtk.mainQuit
     #showAll win 
     Gtk.main
