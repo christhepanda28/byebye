0542a77b6e97ee8ff216c591156d4f3939547dd3
Xander Sanger <xandersangerwork@gmail.com>
2024-12-05 11:40:42 +1100

    update flake syntax error

--- flake.nix
+++ flake.nix
@@ -37,30 +37,36 @@
       })
     // {
       # NixOS module for system-wide installation
-      nixosModules.byebye = { config, lib, pkgs, ... }: 
-      with lib; {
-        options.programs.byebye = {
-          enable = mkEnableOption "ByeBye logout utility";
+      nixosModules.byebye = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+        with lib; {
+          options.programs.byebye = {
+            enable = mkEnableOption "ByeBye logout utility";
 
-          package = mkOption {
-            type = types.package;
-            default = self.packages.x86_64-linux.default;
-            description = "ByeBye package to use";
+            package = mkOption {
+              type = types.package;
+              default = self.packages.x86_64-linux.default;
+              description = "ByeBye package to use";
+            };
+          };
+
+          config = mkIf config.services.byebye.enable {
+            environment.systemPackages = [self.packages.x86_64-linux.default];
           };
         };
 
-        config = mkIf config.services.byebye.enable {
-          environment.systemPackages = [ self.packages.x86_64-linux.default ];
-        };
+      # --- Flake Local Nix Configuration ----------------------------
+      nixConfig = {
+        # This sets the flake to use the IOG nix cache.
+        # Nix should ask for permission before using it,
+        # but remove it here if you do not want it to.
+        extra-substituters = ["https://cache.iog.io"];
+        extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+        allow-import-from-derivation = "true";
       };
-  
-  # --- Flake Local Nix Configuration ----------------------------
-  nixConfig = {
-    # This sets the flake to use the IOG nix cache.
-    # Nix should ask for permission before using it,
-    # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
-    allow-import-from-derivation = "true";
-  };
+    };
 }
